cabdd0d825aae73e63473cc729d6f80e18624767
like <20783506632@qq.com>
2017-08-02 14:53:00 +0800

    jquery ujs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,14 @@ class PostsController < ApplicationController
 
 	end
 
+	def update
+		sleep(1)
+		@post = Post.find(params[:id])
+		@post.update!(post_params)
+
+		render :json => { :id => @post.id, :message => "ok" }
+	end
+
 	def destroy
 		@post = current_user.posts.find(params[:id])
 		@post.destroy
@@ -40,10 +48,23 @@ class PostsController < ApplicationController
       render "like"
 	  end
 
+	  def rate
+    @post = Post.find(params[:id])
+
+    existing_score = @post.find_score(current_user)
+    if existing_score
+      existing_score.update( :score => params[:score] )
+    else
+      @post.scores.create( :score => params[:score], :user => current_user )
+    end
+
+    render :json => { :average_score => @post.average_score }
+  end
+
 
 	protected
 
 	def post_params
-		params.require(:post).permit(:content)
+		params.require(:post).permit(:content, :category_id)
 	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
 	validates_presence_of :content
 	belongs_to :user
+	belongs_to :category, :optional => true
 
 	has_many :likes, :dependent => :destroy
 	has_many :liked_users, :through => :likes, :source => :user
@@ -8,4 +9,15 @@ class Post < ApplicationRecord
 	def find_like(user)
 		self.likes.where( :user_id => user.id ).first
 	end
+
+	 has_many :scores, :class_name => "PostScore"
+
+  def find_score(user)
+    user && self.scores.where( :user_id => user.id ).first
+  end
+
+
+  def average_score
+    self.scores.average(:score)
+  end
 end
