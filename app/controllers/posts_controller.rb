a8f1b5351a67053ffaeb6a1e3413b0134032d4a2
like <20783506632@qq.com>
2017-08-02 10:01:25 +0800

    implement ajax on rails

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,16 @@
 class PostsController < ApplicationController
 	before_action :authenticate_user!, :only => [:create, :destroy]
 	def index
-		@posts = Post.order("id DESC").all
+		 @posts = Post.order("id DESC").limit(20)
+
+    if params[:max_id]
+      @posts = @posts.where( "id < ?", params[:max_id])
+    end
+
+    respond_to do |format|
+      format.html  # 如果客户端要求 HTML，则回传 index.html.erb
+      format.js    # 如果客户端要求 JavaScript，回传 index.js.erb
+    end
 	end
 
 	def create
@@ -14,7 +23,22 @@ class PostsController < ApplicationController
 	def destroy
 		@post = current_user.posts.find(params[:id])
 		@post.destroy
+		render :json => { :id => @post.id }
 	end
+
+	def like
+		@post = Post.find(params[:id])
+		unless @post.find_like(current_user) # 如果已经按讚过了，就略过不再新增
+			Like.create( :user => current_user, :post => @post)
+		end
+	end
+
+	  def unlike
+	  	@post = Post.find(params[:id])
+	  	  like = @post.find_like(current_user)
+	  	  like.destroy
+      render "like"
+	  end
 
 
 	protected
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          has_many :posts
@@ -9,4 +7,7 @@ class User < ApplicationRecord
          	## 取 email 的前半来显示，如果你也可以另开一个字段是 nickname 让用户可以自己编辑显示名称
          	self.email.split("@").first
          end
+
+  has_many :likes, :dependent => :destroy
+  has_many :liked_posts, :through => :likes, :source => :post
 end
